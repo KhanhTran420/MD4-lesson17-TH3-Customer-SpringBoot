7d56e5e5dd0c28f263ea85444464e6d8c05a9da2
unknown <khanhbi1998pt@gmail.com>
2022-05-05 14:04:43 +0700

    TH3 update 5/5/2022

---
+++ src/main/java/com/example/th3/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.th3.service;
+
+import com.example.th3.model.Customer;
+import com.example.th3.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
